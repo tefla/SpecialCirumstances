dc23f7498e057247e08fdc06d8fb5ea795d7e4ab
Timbo <tim@refsix.com>
2024-11-20 08:52:41 +0000

    Added Readme

--- Game.gd
+++ Game.gd
@@ -4,7 +4,16 @@ var selection_started = false
 var selection_start: Vector2
 var selection_end: Vector2
 
-func _input(event: InputEvent) -> void:
+
+func checkMouseModeActions():
+	if Input.is_action_just_pressed("MouseModeNone"):
+		GameManager.set_selection_mode(GameManager.SelectionMode.NONE)
+	elif Input.is_action_just_pressed("MouseModeSingle"):
+		GameManager.set_selection_mode(GameManager.SelectionMode.SINGLE)
+	elif Input.is_action_just_pressed("MouseModeArea"):
+		GameManager.set_selection_mode(GameManager.SelectionMode.AREA)
+		
+func mouseModeRender(event: InputEvent):
 	match GameManager.selection_mode:
 		GameManager.SelectionMode.NONE:
 			pass
@@ -14,11 +23,14 @@ func _input(event: InputEvent) -> void:
 				# Start the drag
 				selection_start = get_global_mouse_position()
 			elif event.is_action_released("LeftMouseButton") and selection_started == true:
-				
+				selection_end = get_global_mouse_position()
 				selection_started = false
 			else:
 				selection_end = get_global_mouse_position()
-			queue_redraw()
+			queue_redraw()	
+func _input(event: InputEvent) -> void:
+	checkMouseModeActions()
+	mouseModeRender(event)
 func _draw() -> void:
 	if selection_started:
 		var width = selection_end.x - selection_start.x
