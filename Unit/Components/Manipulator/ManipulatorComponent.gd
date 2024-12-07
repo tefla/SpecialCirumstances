5c3d0e88461d8a5a00f4a8484acfc65d4195c0f7
Timbo <tim@refsix.com>
2024-12-07 14:59:28 +0000

    moar again!

--- Unit/Components/Manipulator/ManipulatorComponent.gd
+++ Unit/Components/Manipulator/ManipulatorComponent.gd
@@ -1,11 +1,19 @@
 extends Component
 
+func _init() -> void:
+	features = ComponentFeatures.TASK_PROVIDER
+	tasks = [PickUpTask.new()]
+
+
 @onready var area = $Area2D
 func scan_area():
 	var areas = area.get_overlapping_areas()
 	print("Found following areas", areas)
 	var bodies: Array[Node2D] = area.get_overlapping_bodies()
 	print("Found following bodies", bodies)
-	
+	var all_bodies = areas + bodies
+	for body in all_bodies:
+		if body.can_pick_up:
+			print("Yo")
 	return bodies.front()
 	
--- Unit/Components/Manipulator/PickUp/PickUpTask.gd
+++ Unit/Components/Manipulator/PickUp/PickUpTask.gd
@@ -2,12 +2,12 @@ extends Task
 class_name PickUpTask
 
 func _init() -> void:
-	name = "Position"
+	name = "Pick Up"
 	block = preload("res://Tasks/Blocks/BlockFunction.tscn")
 	result_type = ResultType.NONE
 
 func run():
 	running()
 	await wait()
-	
+	component.scan_area()
 	success()
