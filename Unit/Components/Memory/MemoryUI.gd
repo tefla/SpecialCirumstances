13de42d81704c54227d2c2401f62220b4696339f
tim <tim@refsix.com>
2024-12-08 15:35:45 +0000

    moore

--- Unit/Components/Memory/MemoryUI.gd
+++ Unit/Components/Memory/MemoryUI.gd
@@ -3,11 +3,20 @@ class_name MemoryUI
 @onready var palette = %Palette
 @onready var program_editor = %ProgramEditor
 func _ready() -> void:
+	print(program_editor.root)
+	var root = ResourceLoader.load("user://tree.res")
+	if root is Task:
+		program_editor.root = root
+		program_editor.redraw()
 	pass
 func _on_step_pressed() -> void:
 	%ProgramEditor.root.run()
 
 
 func _on_reset_pressed() -> void:
-	print(program_editor.root.component.entity)
+	var error = ResourceSaver.save(program_editor.root, "user://tree.res")
+	if error != OK:
+		printerr(error)
+		
+
 	program_editor.root.start()
